76d4c90fcc8d29a88a18c5cd4ab637adc37786b4
eleemebe <lauraborlaff@gmail.com>
2019-04-22 21:02:36 +0200

    Added convert_data function to convert all datafiles

--- main.R
+++ main.R
@@ -1,47 +1,9 @@
+library(readxl)
 library(caret)
 
 base_directory <- "~/Documents/TFM"
 datasets_dir <- "DATASETS"
 setwd(paste0(base_directory, "/", datasets_dir))
-
-# Function to find all the files that contains the data and the information about the data to use.
-# @return: dataframe containing the dataset identifier, the path of its metadata file and the path to the dataset.
-#          The returned paths are the relative paths to the working directory path, that should be set previously.
-locate_input_files <- function(){
-  files <- list.files(path = ".", recursive = TRUE)
-  
-  df_files <- data.frame(matrix(ncol=3, nrow = 0))
-  colnames(df_files) <- c("dataset_id", "dataset_metadata", "dataset_data")
-  
-  
-  for(file in files){
-    print(file)
-    
-    if(grepl("METADATA", file)){
-      
-      dataset_id <- strsplit(file, "/")[[1]][1]
-      df_files[nrow(df_files)+1, ] <- list(dataset_id = dataset_id, dataset_metadata = "", dataset_data= "")
-      
-      df_files[df_files[,'dataset_id']==dataset_id,'dataset_metadata'] <- file
-      
-      metadata_df <- parse_metadata(file)
-      dataset_data <- metadata_df[which(metadata_df$key=='data'),'value']
-      df_files[df_files[,'dataset_id']==dataset_id,'dataset_data'] <- paste0(dataset_id, "/", dataset_data)
-      # lines <- readLines(file, warn=FALSE)
-      # for(line in lines){
-      #   if(grepl("data:", line)){
-      #     dataset_data <- strsplit(line, " ")[[1]][2]
-      #     df_files[df_files[,'dataset_id']==dataset_id,'dataset_data'] <- paste0(dataset_id, "/", dataset_data)
-      #   }
-      # }
-      
-      
-    }
-  }
-  return(df_files)
-}
-  
-input_file_df <- locate_input_files()
 
 #
 #
@@ -53,18 +15,104 @@ parse_metadata <- function(metadata_file_path) {
   colnames(metadata_df) <- c("key", "value")
   lines <- readLines(metadata_file_path, warn=FALSE)
   for(line in lines){
-    print(line)
     field_key <- strsplit(line, ":")[[1]][1]
     field_value <- trimws(strsplit(line, ":")[[1]][2])
-    print(field_value)
     metadata_df[nrow(metadata_df)+1, ] <- list(key = field_key, value = field_value)
   } 
   return(metadata_df)
 }
-metadata_df <- parse_metadata(input_file_df[1,2])
 
+# Function to find all the files that contains the data and the information about the data to use.
+# @return: dataframe containing the dataset identifier, the path of its metadata file and the path to the dataset.
+#          The returned paths are the relative paths to the working directory path, that should be set previously.
+locate_input_files <- function(){
+  files <- list.files(path = ".", recursive = TRUE)
+  
+  df_files <- data.frame(matrix(ncol=4, nrow = 0))
+  colnames(df_files) <- c("dataset_id", "dataset_metadata", "dataset_data", "external_label_data")
+  
+  
+  for(file in files){
+    if(grepl("METADATA", file)){
+      dataset_id <- strsplit(file, "/")[[1]][1]
+      df_files[nrow(df_files)+1, ] <- list(dataset_id = dataset_id, dataset_metadata = "", dataset_data= "")
+      
+      df_files[df_files[,'dataset_id']==dataset_id,'dataset_metadata'] <- file
+      
+      metadata_df <- parse_metadata(file)
+      dataset_data <- metadata_df[which(metadata_df$key=='data'),'value']
+      df_files[df_files[,'dataset_id']==dataset_id,'dataset_data'] <- paste0(dataset_id, "/", dataset_data)
+      
+      external_label_data <- metadata_df[which(metadata_df$key=='labels'),'value']
+      print(external_label_data)
+      if(length(external_label_data)>0){
+        df_files[df_files[,'dataset_id']==dataset_id,'external_label_data'] <- paste0(dataset_id, "/", external_label_data)
+      }
+      else{
+        df_files[df_files[,'dataset_id']==dataset_id,'external_label_data'] <- ""
+      }
+    }
+  }
+  return(df_files)
+}
 
+load_data <- function(metadata_df, data_file) {
+  format <- metadata_df[which(metadata_df$key=='format'),'value']
+  sep=""
+  if (format=="xlsx") {
+    raw_data <- as.data.frame(read_excel(data_file))
+  }else if (format=='comma'){
+    sep=","
+    raw_data <- read.csv2(data_file, header = F, sep = sep)
+  }else if (format=='tab'){
+    sep="\t"
+    raw_data <- read.csv2(data_file, header = F, sep = sep)
+  }else if (format=="semicolon"){
+    sep=";"
+    raw_data <- read.csv2(data_file, header = F, sep = sep)
+  }else if (format=="space") {
+    sep=""
+    raw_data <- read.csv2(data_file, header = F, sep = sep)
+  }else{
+    print("Format not captured")
+    print(format)
+  }
+  return(raw_data)
+}
 
+convert_data <- function(metadata_df, loaded_data, external_labels_data = data.frame()) {
+  columns_df <- metadata_df[which(grepl("column", metadata_df$key)),]
+  preprocessed_df = loaded_data
+  if(sum(columns_df['key'] == 'column_all')==1){
+    column_type <- metadata_df[which(metadata_df['key'] == 'column_all'),'value']
+    if(column_type == "numerical"){
+      preprocessed_df <- data.frame(lapply(loaded_data, function(x) as.numeric(x)))
+    } else {
+      preprocessed_df <- data.frame(lapply(loaded_data, function(x) as.factor(x)))
+    }
+    preprocessed_df$target <- factor(loaded_external_label_data[,])
+  } else {
+    for (i in 1:nrow(columns_df)) {
+      column_index <- i
+      column_type <- metadata_df[which(metadata_df['key'] == paste0('column_',column_index)),'value']
+      if(column_type == "numerical"){
+        preprocessed_df[,column_index] <- as.numeric(loaded_data[,column_index])
+      } else if(column_type == "categorical"){
+        preprocessed_df[,column_index] <- factor(loaded_data[,column_index])
+      } else if(column_type == "target"){
+        preprocessed_df[,column_index] <- factor(loaded_data[,column_index])
+        colnames(preprocessed_df)[column_index] <- "target"
+      }
+    }
+  }
+  return(preprocessed_df)
+}
+
+input_file_df <- locate_input_files()
+metadata_df <- parse_metadata(input_file_df[3,2])
+loaded_data <- load_data(metadata_df, input_file_df[3,3])
+loaded_external_label_data <- load_data(metadata_df, input_file_df[3,4])
+converted_df <- convert_data(metadata_df, loaded_data, loaded_external_label_data)
 
 for (i in 1:length(df_files[,'dataset_data'])) {
 #for (i in 1:1) {
@@ -104,3 +152,61 @@ for (i in 1:length(df_files[,'dataset_data'])) {
   }
   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+" Falta:
+1. bajar hasta 20 datasets y elegir 10 modelos 
+
+datasets: googledrive folder tfm
+modelos: rf/ranger, svmLinear, svmPoly, glm, rpart, xgbDART/xgbLinear, ada, nb, knn, mxnet
+
+-> DONE
+
+2. crear archivo metadata para cada dataset para categorizar variables como categoricas/numericas
+añadir un archivo METADATA a cada uno de los datasets para indicar el tipo de atributo que es: numerical, categorical o target.
+
+-> DONE
+
+
+3. ajustar el trainControl para los modelos que elijamos
+rf/ranger -> rf con mtry 2:length(dataset_columns)
+svmLinear -> C (10^-5, 10^-3, 10^-1, 10^1, 10^3 o 10^5)
+svmPoly -> degree: (0 a 6), scale: (10^-15, 10^-13, 10^-11, 10^-9, ..., 10^3), C: (10^-5, 10^-3, 10^-1, 10^1, 10^3 o 10^5)
+glm -> none
+rpart -> cp = 0 a 1 exponencialmente valores mas cercanos a cero
+xgbDART/xgbLinear -> xgbLinear - nrounds = (2), lambda=(0 a 1), alpha=(0 a 1), eta=(0.01, 0.001, 0.0001), 
+ada -> iter=(20, 50 100, 500), maxdepth =(1 a 5), nu=(0 a 1)
+nb -> fL = (0 a 1,2,3,4,5), usekernel= TRUE or False, adjust=(0 a 5)
+knn -> k =(1 a 20)
+mxnet -> layer1 =(64, 128, 256), layer2=(16, 32, 64), layer3=(2,4,8,16), learning.rate=(10^-5 a 10 exponencialmente), momentum=(0.9), dropout=(0 a 0,5), activation=('relu')
+
+4. Codificarlo en R:
+  4.1: prepareara los data files
+  4.2: codificiar el entrenamineto de los modelos 
+
+5. Presentar resultados en funcion de: 
+5.1 Paralelizable o no
+5.2 Datasets con mayoria de varialbes categoricas
+5.3 Datasets con mayoria de varialbes numericas
+5.4 por tamaño de dataset
+5.5 Por numero de variables 
+
+6. Escribir memo
+"
